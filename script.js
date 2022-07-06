843e2222d504b0313445b00c9c7e922579714a65
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-06 16:21:59 +0100

    Added card rendering, basic search functionality

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
 import {pokemonArray} from './data/pokemon.js'
 const cardContainer = document.querySelector(".card-container")
+const searchBarText = document.querySelector("#search-bar")
+const searchBarButton = document.querySelector("#search-button");
 // pokemonArray is an array of objects.
 
+pokemonArray.forEach(pokemon => {
+    pokemon.name = pokemon.name[0].toUpperCase() + pokemon.name.substring(1)
+})
+
 const handleCardRender = (event) => {
-    for (i=0; i<pokemonArray.length; i++) {
-        cardContainer.innerHTML += `<card class="card">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" alt="" class="card__image">
+  pokemonArray.forEach((pokemon) => {
+    if (pokemon.types[1] !== undefined) {
+      cardContainer.innerHTML += `<card class="card">
+        <img src="${pokemon.sprite}" alt="" class="card__image">
         <div class="card__content">
-          <h3 class="card__heading">Ivysaur</h3>
-          <p class="card__text"> ${pokemon.name} (${pokemon.id}) is a ${pokemon.types[0]} & ${pokemon.types[1]} type pokemon</p>
+          <h3 class="card__heading">${pokemon.name}</h3>
+          <p class="card__text"> ${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} & ${pokemon.types[1]} type pokemon</p>
+        </div>
+      </card>`;
+    } else {
+      cardContainer.innerHTML += `<card class="card">
+        <img src="${pokemon.sprite}" alt="" class="card__image">
+        <div class="card__content">
+          <h3 class="card__heading">${pokemon.name}</h3>
+          <p class="card__text"> ${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} type pokemon</p>
         </div>
       </card>`;
     }
+  });
+};
+
+const handleSearch = (event) => {
+    console.log(searchBarText.value)
+    pokemonArray.forEach(pokemon => {
+        if (searchBarText.value == pokemon.name) {
+          if (pokemon.types[1] !== undefined) {
+            cardContainer.innerHTML = `<card class="card">
+        <img src="${pokemon.sprite}" alt="" class="card__image">
+        <div class="card__content">
+          <h3 class="card__heading">${pokemon.name}</h3>
+          <p class="card__text"> ${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} & ${pokemon.types[1]} type pokemon</p>
+        </div>
+      </card>`;
+          } else {
+            cardContainer.innerHTML = `<card class="card">
+        <img src="${pokemon.sprite}" alt="" class="card__image">
+        <div class="card__content">
+          <h3 class="card__heading">${pokemon.name}</h3>
+          <p class="card__text"> ${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} type pokemon</p>
+        </div>
+      </card>`;
+          }
+        }
+    })
 }
 
-document.addEventListener("load", handleCardRender)
 
+
+handleCardRender()
+searchBarButton.addEventListener("click", handleSearch)
