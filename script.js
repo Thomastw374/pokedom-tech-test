ac86351987fec09f5006c872de7b968578f88bcc
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-06 16:51:37 +0100

    commented out results num code in case not func by deadline

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ const searchBarText = document.querySelector("#search-bar")
 const searchBarButton = document.querySelector("#search-button");
 const resultNumberDropdown = document.querySelector("#results-dropdown");
 
-function showNoOfResults(number) {
-    for(let i=0; i<number; i++) {
-        if (pokemon.types[1] !== undefined) {
-          cardContainer.innerHTML += `<card class="card">
-        <img src="${pokemon.sprite}" alt="" class="card__image">
-        <div class="card__content">
-          <h3 class="card__heading">${pokemon.name}</h3>
-          <p class="card__text"> ${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} & ${pokemon.types[1]} type pokemon</p>
-        </div>
-      </card>`;
-        } else {
-          cardContainer.innerHTML += `<card class="card">
-        <img src="${pokemon.sprite}" alt="" class="card__image">
-        <div class="card__content">
-          <h3 class="card__heading">${pokemon.name}</h3>
-          <p class="card__text"> ${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} type pokemon</p>
-        </div>
-      </card>`;
-        }
-    }
-}
+// function showNoOfResults(number) {
+//     for(let i=0; i<number; i++) {
+//         if (pokemon.types[1] !== undefined) {
+//           cardContainer.innerHTML += `<card class="card">
+//         <img src="${pokemon.sprite}" alt="" class="card__image">
+//         <div class="card__content">
+//           <h3 class="card__heading">${pokemon.name}</h3>
+//           <p class="card__text"> ${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} & ${pokemon.types[1]} type pokemon</p>
+//         </div>
+//       </card>`;
+//         } else {
+//           cardContainer.innerHTML += `<card class="card">
+//         <img src="${pokemon.sprite}" alt="" class="card__image">
+//         <div class="card__content">
+//           <h3 class="card__heading">${pokemon.name}</h3>
+//           <p class="card__text"> ${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} type pokemon</p>
+//         </div>
+//       </card>`;
+//         }
+//     }
+// }
 
 pokemonArray.forEach(pokemon => {
     pokemon.name = pokemon.name[0].toUpperCase() + pokemon.name.substring(1)
@@ -77,11 +77,11 @@ const handleSearch = (event) => {
     })
 }
 
-const handleResultNumber = (event) => {
-    showNoOfResults(resultNumberDropdown.value)
-}
+// const handleResultNumber = (event) => {
+//     showNoOfResults(resultNumberDropdown.value)
+// }
 
 
 handleCardRender()
 searchBarButton.addEventListener("click", handleSearch)
-resultNumberDropdown.addEventListener("click", handleResultNumber)
+// resultNumberDropdown.addEventListener("click", handleResultNumber)
